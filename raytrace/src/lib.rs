e1323fb3e9766b90dde4bc97ae7d1a6244be9068
josh <josh.reynolds@frontier.com>
2024-03-12 12:21:04 -0700

    First test - basic Tuple implementation

---
+++ raytrace/src/lib.rs
@@ -0,0 +1,36 @@
+// note - should use an epsilon value for comparing two floats
+//
+pub struct Tuple {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+    pub w: f64,
+}
+
+impl Tuple {
+    pub fn is_point(&self) -> bool {
+        self.w == 1.0
+    }
+
+    pub fn is_vector(&self) -> bool {
+        self.w == 0.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tuple_with_w_1_is_point(){
+        let a = Tuple { x: 4.3, y: -4.2, z: 3.1, w: 1.0 };
+        assert!(
+            a.x == 4.3 &&
+            a.y == -4.2 &&
+            a.z == 3.1 &&
+            a.w == 1.0 &&
+            a.is_point() &&
+            !a.is_vector()
+        );
+    }
+}
