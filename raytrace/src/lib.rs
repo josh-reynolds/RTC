5fd697ece45a9a33c007a7785f7edc82375844d7
josh <josh.reynolds@frontier.com>
2024-03-12 13:54:06 -0700

    Replacing float equality tests with equals()

--- raytrace/src/lib.rs
+++ raytrace/src/lib.rs
@@ -9,24 +9,26 @@ pub struct Tuple {
 
 impl Tuple {
     pub fn is_point(&self) -> bool {
-        self.w == 1.0
+        equals( self.w, 1.0 )
     }
 
     pub fn is_vector(&self) -> bool {
-        self.w == 0.0
+        equals( self.w, 0.0 )
     }
 
     pub fn point(x: f64, y: f64, z: f64) -> Tuple {
-        Tuple { x, y, z, w: 1.0 }
+        Self { x, y, z, w: 1.0 }
     }
 
     pub fn vector(x: f64, y: f64, z: f64) -> Tuple {
-        Tuple { x, y, z, w: 0.0 }
+        Self { x, y, z, w: 0.0 }
     }
 
     pub fn equals(&self, t: Tuple) -> bool {
-        self.x == t.x && self.y == t.y &&
-        self.z == t.z && self.w == t.w
+        equals( self.x, t.x ) && 
+        equals( self.y, t.y ) &&
+        equals( self.z, t.z ) && 
+        equals( self.w, t.w )
     }
 }
 
@@ -41,10 +43,10 @@ mod tests {
     fn tuple_with_w_1_is_point(){
         let a = Tuple { x: 4.3, y: -4.2, z: 3.1, w: 1.0 };
         assert!(
-            a.x == 4.3 &&
-            a.y == -4.2 &&
-            a.z == 3.1 &&
-            a.w == 1.0 &&
+            equals( a.x, 4.3 ) &&
+            equals( a.y, -4.2 ) &&
+            equals( a.z, 3.1 ) &&
+            equals( a.w, 1.0 ) &&
             a.is_point() &&
             !a.is_vector()
         );
@@ -54,10 +56,10 @@ mod tests {
     fn tuple_with_w_0_is_vector(){
         let a = Tuple { x: 4.3, y: -4.2, z: 3.1, w: 0.0 };
         assert!(
-            a.x == 4.3 &&
-            a.y == -4.2 &&
-            a.z == 3.1 &&
-            a.w == 0.0 &&
+            equals( a.x, 4.3 ) &&
+            equals( a.y, -4.2 ) &&
+            equals( a.z, 3.1 ) &&
+            equals( a.w, 0.0 ) &&
             !a.is_point() &&
             a.is_vector()
         );
@@ -74,6 +76,8 @@ mod tests {
     fn vector_creates_vectors(){
         // should allow integers as well
         let a = Tuple::vector(4.0, -4.0, 3.0);
+        assert!( a.equals(Tuple { x: 4.0, y: -4.0, z: 3.0, w: 0.0 }));
+
     }
 
     #[test]
