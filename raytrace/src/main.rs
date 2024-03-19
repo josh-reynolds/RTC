62abb54afb209617b06a5588bead62efd3d3e857
josh-reynolds <josh.reynolds@frontier.com>
2024-03-19 17:26:48 -0400

    Initial structs for simple simulation project

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -1,3 +1,17 @@
+use raytrace::Tuple;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug)]
+pub struct Projectile {
+    pub posn: Tuple,
+    pub vel: Tuple,
+}
+
+#[derive(Debug)]
+pub struct Environment {
+    pub gravity: Tuple,
+    pub wind: Tuple,
 }
