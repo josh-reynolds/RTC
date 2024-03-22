ffa6c572b7fb29cdfe471f08ba141bf1fe641fcb
josh-reynolds <josh.reynolds@frontier.com>
2024-03-22 17:15:19 -0400

    Finishing simulation from end of chapter 1

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -2,20 +2,24 @@ use raytrace::Tuple;
 use raytrace::Number;
 
 fn main() {
-    println!("Hello, world!");
+    let mut p = Projectile{ posn: Tuple::point(Number::from(0), 
+                                               Number::from(1), 
+                                               Number::from(0)),
+                            vel: Tuple::vector(Number::from(1), 
+                                               Number::from(1), 
+                                               Number::from(0)).normal() };
 
-    let p = Projectile{ posn: Tuple::point(Number::from(0), 
-                                           Number::from(1), 
-                                           Number::from(0)),
-                        vel: Tuple::vector(Number::from(1), 
-                                           Number::from(1), 
-                                           Number::from(0)).normal() };
     let e = Environment{ gravity: Tuple::vector(Number::from(0), 
                                                 Number::from(-0.1), 
                                                 Number::from(0)),
                          wind: Tuple::vector(Number::from(-0.01),
                                              Number::from(0),
                                              Number::from(0)) };
+    
+    while p.posn.y > 0.0 {
+       p = tick(&e, &p);
+       println!("{:?}", p);
+    }
 }
 
 #[derive(Debug)]
@@ -30,7 +34,7 @@ pub struct Environment {
     pub wind: Tuple,
 }
 
-fn tick(e: Environment, p: Projectile) -> Projectile {
+fn tick(e: &Environment, p: &Projectile) -> Projectile {
     let posn = p.posn.plus(&p.vel);
     let vel = p.vel.plus(&e.gravity).plus(&e.wind);
     Projectile{ posn: posn, vel: vel }
