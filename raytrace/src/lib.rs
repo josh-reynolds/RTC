4e426345ad7741e9684189a09511450e154cb3b5
josh-reynolds <josh.reynolds@frontier.com>
2024-03-23 17:43:58 -0400

    Adding Color struct

--- raytrace/src/lib.rs
+++ raytrace/src/lib.rs
@@ -103,6 +103,13 @@ impl Tuple {
     }
 }
 
+#[derive(Debug)]
+pub struct Color {
+    pub r: f64,
+    pub g: f64,
+    pub b: f64,
+}
+
 pub struct Number {
     pub value: f64,
 }
@@ -388,4 +395,12 @@ mod tests {
                                                   Number::from(-2),
                                                   Number::from(1))));
     }    
+
+    #[test]
+    fn colors_are_r_g_b_tuples(){
+        let c = Color { r: -0.5, g: 0.4, b: 1.7 };
+        assert!( equals(c.r, -0.5) &&
+                 equals(c.g, 0.4) &&
+                 equals(c.b, 1.7));
+    }
 }
