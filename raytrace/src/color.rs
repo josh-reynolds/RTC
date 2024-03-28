526b687072d813ca3d5ad10aec22290a02ed4cdc
josh-reynolds <josh.reynolds@frontier.com>
2024-03-28 16:10:26 -0400

    Adding implementation of plus() and equals() to Color - some duplication here with Tuple

--- raytrace/src/color.rs
+++ raytrace/src/color.rs
@@ -1,9 +1,24 @@
+use crate::equals;
 
 #[derive(Debug)]
 pub struct Color {
     pub r: f64,
     pub g: f64,
     pub b: f64,
+}
+
+impl Color {
+    pub fn plus(&self, c: &Color) -> Self {
+        Self { r: self.r + c.r,
+               g: self.g + c.g,
+               b: self.b + c.b, }
+    }
+
+    pub fn equals(&self, c: Color) -> bool {
+        equals( self.r, c.r ) && 
+        equals( self.g, c.g ) &&
+        equals( self.b, c.b ) 
+    }
 }
 
 #[cfg(test)]
@@ -23,7 +38,6 @@ mod tests {
     fn adding_colors(){
         let c1 = Color { r: 0.9, g: 0.6, b: 0.75 };
         let c2 = Color { r: 0.7, g: 0.1, b: 0.25 };
-        assert!( c1.plus(&c2).equals(Color { r: 0.7, g: 0.1, b: 0.25 }));
-
+        assert!( c1.plus(&c2).equals(Color { r: 1.6, g: 0.7, b: 1.0 }));
     }
 }
