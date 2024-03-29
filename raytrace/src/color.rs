fcb8287337af5907bece7cea6ac3b91a18092115
josh-reynolds <josh.reynolds@frontier.com>
2024-03-29 16:27:50 -0400

    Adding test and implementation for Color.sub()

--- raytrace/src/color.rs
+++ raytrace/src/color.rs
@@ -14,6 +14,12 @@ impl Color {
                b: self.b + c.b, }
     }
 
+    pub fn sub(&self, c: &Color) -> Self {
+        Self { r: self.r - c.r,
+               g: self.g - c.g,
+               b: self.b - c.b, }
+    }
+    
     pub fn equals(&self, c: Color) -> bool {
         equals( self.r, c.r ) && 
         equals( self.g, c.g ) &&
@@ -39,5 +45,12 @@ mod tests {
         let c1 = Color { r: 0.9, g: 0.6, b: 0.75 };
         let c2 = Color { r: 0.7, g: 0.1, b: 0.25 };
         assert!( c1.plus(&c2).equals(Color { r: 1.6, g: 0.7, b: 1.0 }));
+    }
+    
+    #[test]
+    fn subtracting_colors(){
+        let c1 = Color { r: 0.9, g: 0.6, b: 0.75 };
+        let c2 = Color { r: 0.7, g: 0.1, b: 0.25 };
+        assert!( c1.sub(&c2).equals(Color { r: 0.2, g: 0.5, b: 0.5 }));
     }
 }
