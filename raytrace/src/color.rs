3aae5ec9bdbd28ad11d0c1c91c83e416417e251e
josh-reynolds <josh.reynolds@frontier.com>
2024-03-30 20:13:14 -0400

    Adding test & implementation for Color.multC()

--- raytrace/src/color.rs
+++ raytrace/src/color.rs
@@ -33,6 +33,12 @@ impl Color {
                g: self.g * n,
                b: self.b * n }
     }
+
+    pub fn multC(&self, c: Color) -> Self {
+        Self { r: self.r * c.r,
+               g: self.g * c.g,
+               b: self.b * c.b }
+    }
 }
 
 #[cfg(test)]
@@ -54,7 +60,7 @@ mod tests {
         let c2 = Color { r: 0.7, g: 0.1, b: 0.25 };
         assert!( c1.plus(&c2).equals(Color { r: 1.6, g: 0.7, b: 1.0 }));
     }
-    
+
     #[test]
     fn subtracting_colors(){
         let c1 = Color { r: 0.9, g: 0.6, b: 0.75 };
@@ -67,5 +73,12 @@ mod tests {
     fn multipy_color_by_scalar(){
         let c = Color { r: 0.2, g: 0.3, b: 0.4 };
         assert!( c.mult(2.0).equals( Color { r: 0.4, g: 0.6, b: 0.8 } ));
+    }
+
+    #[test]
+    fn multiply_color_by_color(){
+        let c1 = Color { r: 1.0, g: 0.2, b: 0.4 };
+        let c2 = Color { r: 0.9, g: 1.0, b: 0.1 };
+        assert!( c1.multC(c2).equals( Color { r: 0.9, g: 0.2, b: 0.04 } ));
     }
 }
