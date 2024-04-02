0d492cb4204e55fdfd0aa0de65ddc0a1a9fd5663
josh-reynolds <josh.reynolds@frontier.com>
2024-04-01 22:20:14 -0400

    Adding multidimensional vector to Canvas struct to hold pixel data

--- raytrace/src/canvas.rs
+++ raytrace/src/canvas.rs
@@ -1,18 +1,27 @@
+use crate::color::Color;
 
 #[derive(Debug)]
 pub struct Canvas {
     pub height: i32,
     pub width: i32,
+    pub pixels: Vec<Vec<Color>>,
 }
 
 
 #[cfg(test)]
 mod tests {
     use crate::canvas::Canvas;
+    use crate::color::Color;
 
     #[test]
     fn canvas_has_width_and_height(){
-        let c = Canvas { width: 10, height: 20 };
+        let c = Canvas { width: 10, height: 20, pixels: vec![vec![Color{r:0.0,g:0.0,b:0.0}]]};
         assert!( (c.width == 10) && (c.height == 20) );
+    }
+
+    #[test]
+    fn canvas_1_1_has_1_pixel(){
+        let c = Canvas { width: 1, height: 1, pixels: vec![vec![Color{r:0.0,g:0.0,b:0.0}]] };
+
     }
 }
