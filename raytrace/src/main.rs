31ac05b115d70c792f188a561e2fb0870e6f9918
josh-reynolds <josh.reynolds@frontier.com>
2024-04-25 16:43:24 -0400

    Implementing the 'cannon' project from the end of Chapter 2 - still hitting an overrun at the end that needs debugging

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -1,25 +1,39 @@
 use raytrace::number::Number;
 use raytrace::tuple::Tuple;
+use raytrace::canvas::Canvas;
+use raytrace::color::Color;
 
 fn main() {
-    let mut p = Projectile{ posn: Tuple::point(Number::from(0), 
-                                               Number::from(1), 
-                                               Number::from(0)),
-                            vel: Tuple::vector(Number::from(1), 
-                                               Number::from(1), 
-                                               Number::from(0)).normal() };
+    let mut c = Canvas::new(900, 550);
 
-    let e = Environment{ gravity: Tuple::vector(Number::from(0), 
-                                                Number::from(-0.1), 
-                                                Number::from(0)),
-                         wind: Tuple::vector(Number::from(-0.01),
-                                             Number::from(0),
-                                             Number::from(0)) };
+    let start = Tuple::point(Number::from(0),
+                             Number::from(1),
+                             Number::from(1));
+    let velocity = Tuple::vector(Number::from(1),
+                                 Number::from(1.8),
+                                 Number::from(0)).normal().mult(11.25);
+    let mut p = Projectile{ posn: start, vel: velocity };
+
+    let gravity = Tuple::vector(Number::from(0),
+                                Number::from(-0.1),
+                                Number::from(0));
+    let wind = Tuple::vector(Number::from(-0.01),
+                             Number::from(0),
+                             Number::from(0));
+    let e = Environment{ gravity: gravity, wind: wind };
     
+    let red = Color{r:1.0,g:0.0,b:0.0};
     while p.posn.y > 0.0 {
        p = tick(&e, &p);
-       println!("{:?}", p);
+       let x = p.posn.x as i32;
+       let y = (c.height as f64 - p.posn.y) as i32;
+       println!("{},{}", x, y);
+       c.write_pixel(x.try_into().unwrap(),
+                     y.try_into().unwrap(),
+                     red);
     }
+    
+    let _ = c.to_ppm("cannon.ppm");
 }
 
 #[derive(Debug)]
