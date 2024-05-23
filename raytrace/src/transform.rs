b6e425c02365c3021b4581449f9f650343ffb952
josh-reynolds <josh.reynolds@frontier.com>
2024-05-23 16:30:32 -0700

    Implementation for transform::translation() - and fixing an error in the test case

--- raytrace/src/transform.rs
+++ raytrace/src/transform.rs
@@ -1,7 +1,11 @@
 use crate::matrix::Matrix;
 
-pub fn translation(_x: f64, _y: f64, _z: f64) -> Matrix {
-    Matrix::identity()
+pub fn translation(x: f64, y: f64, z: f64) -> Matrix {
+    let mut t = Matrix::identity();
+    t.set(0,3,x);
+    t.set(1,3,y);
+    t.set(2,3,z);
+    t
 }
 
 #[cfg(test)]
@@ -14,9 +18,10 @@ mod tests {
     #[test]
     fn multiply_by_translation_matrix(){
         let t = translation(5.0, -3.0, 2.0);
-        let p = Tuple::point(Number::from(-1), 
+        let p = Tuple::point(Number::from(-3), 
                              Number::from(4), 
                              Number::from(5));
+
         assert!( t.multup(&p).equals( Tuple::point(Number::from(2), 
                                                    Number::from(1), 
                                                    Number::from(7))))
