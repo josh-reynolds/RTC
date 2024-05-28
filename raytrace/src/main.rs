0920fabc96622037d0ef3925ae3c94bc5756d9ac
josh-reynolds <josh.reynolds@frontier.com>
2024-05-28 16:38:26 -0700

    Implementing the clock face project from the end of chapter 4

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -2,56 +2,28 @@ use raytrace::number::Number;
 use raytrace::tuple::Tuple;
 use raytrace::canvas::Canvas;
 use raytrace::color::Color;
+use raytrace::transform::*;
+use std::f64::consts::PI;
 
 fn main() {
-    let mut c = Canvas::new(900, 550);
-
-    let start = Tuple::point(Number::from(0),
-                             Number::from(1),
-                             Number::from(1));
-    let velocity = Tuple::vector(Number::from(1),
-                                 Number::from(1.8),
-                                 Number::from(0)).normal().mult(11.25);
-    let mut p = Projectile{ posn: start, vel: velocity };
-
-    let gravity = Tuple::vector(Number::from(0),
-                                Number::from(-0.1),
-                                Number::from(0));
-    let wind = Tuple::vector(Number::from(-0.01),
-                             Number::from(0),
-                             Number::from(0));
-    let e = Environment{ gravity: gravity, wind: wind };
-    
+    let mut c = Canvas::new(400, 400);
     let red = Color{r:1.0,g:0.0,b:0.0};
-    while p.posn.y > 0.0 {
-       p = tick(&e, &p);
-       let x = p.posn.x as i32;
-       let mut y = (c.height as f64 - p.posn.y) as i32;
-       if y > c.height as i32 {
-           y = c.height as i32 - 1;
-       }
-       c.write_pixel(x.try_into().unwrap(),
-                     y.try_into().unwrap(),
-                     red);
+
+    let p = Tuple::point(Number::from(0),
+                         Number::from(0),
+                         Number::from(0));
+    
+    let clock_size = translation(100.0,0.0,0.0);
+    let centering = translation(200.0,200.0,0.0);
+
+    for count in 0..12 {
+        let hour = rotation_z((PI / 6.0) * count as f64);
+        let p2 = centering.mult( &hour.mult( &clock_size )).multup(&p);
+        c.write_pixel((p2.x as i32).try_into().unwrap(),
+                      (p2.y as i32).try_into().unwrap(),
+                      red);
     }
     
-    let _ = c.to_ppm("cannon.ppm");
+    let _ = c.to_ppm("clock.ppm");
 }
 
-#[derive(Debug)]
-pub struct Projectile {
-    pub posn: Tuple,
-    pub vel: Tuple,
-}
-
-#[derive(Debug)]
-pub struct Environment {
-    pub gravity: Tuple,
-    pub wind: Tuple,
-}
-
-fn tick(e: &Environment, p: &Projectile) -> Projectile {
-    let posn = p.posn.plus(&p.vel);
-    let vel = p.vel.plus(&e.gravity).plus(&e.wind);
-    Projectile{ posn: posn, vel: vel }
-}
