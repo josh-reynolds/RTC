0c30ce7f470deaa51a8de566fc2f7b3ce8938215
josh-reynolds <josh.reynolds@frontier.com>
2024-05-30 17:28:38 -0700

    Adding test and implementation for Ray.position()

--- raytrace/src/rays.rs
+++ raytrace/src/rays.rs
@@ -10,6 +10,10 @@ impl Ray {
     pub fn new(origin: Tuple, direction: Tuple) -> Self {
         Self { origin: origin, direction: direction }
     }
+
+    pub fn position(&self, distance: f64) -> Tuple {
+        self.origin.plus( &self.direction.mult( distance ))
+    }
 }
 
 #[cfg(test)]
@@ -34,5 +38,30 @@ mod tests {
         assert!( r.direction.equals(Tuple::vector( Number::from(4),
                                                    Number::from(5),
                                                    Number::from(6))) );
+    }
+
+    #[test]
+    fn computing_point_from_distance(){
+        let o = Tuple::point( Number::from(2),
+                              Number::from(3),
+                              Number::from(4));
+        let d = Tuple::vector( Number::from(1),
+                               Number::from(0),
+                               Number::from(0));
+
+        let r = Ray::new(o, d);
+        assert!( r.position(0.0).equals(Tuple::point( Number::from(2),
+                                                      Number::from(3),
+                                                      Number::from(4))) );
+        assert!( r.position(1.0).equals(Tuple::point( Number::from(3),
+                                                      Number::from(3),
+                                                      Number::from(4))) );
+        assert!( r.position(-1.0).equals(Tuple::point( Number::from(1),
+                                                       Number::from(3),
+                                                       Number::from(4))) );
+        assert!( r.position(2.5).equals(Tuple::point( Number::from(4.5),
+                                                      Number::from(3),
+                                                      Number::from(4))) );
+
     }
 }
