6c769f36600ff2398bfc18c64985d0838e828f77
josh-reynolds <josh.reynolds@frontier.com>
2024-06-08 19:18:33 -0700

    Adding test and implementation for Ray.transform()

--- raytrace/src/rays.rs
+++ raytrace/src/rays.rs
@@ -1,4 +1,5 @@
 use crate::tuple::Tuple;
+use crate::matrix::Matrix;
 
 #[derive(Debug)]
 pub struct Ray {
@@ -14,6 +15,11 @@ impl Ray {
     pub fn position(&self, distance: f64) -> Tuple {
         self.origin.plus( &self.direction.mult( distance ))
     }
+
+    pub fn transform(&self, t: Matrix) -> Self {
+        Ray::new( t.multup( &self.origin ),
+                  t.multup( &self.direction ))
+    }
 }
 
 #[cfg(test)]
@@ -21,6 +27,7 @@ mod tests {
     use crate::rays::Ray;
     use crate::tuple::Tuple;
     use crate::number::Number;
+    use crate::transform::translation;
 
     #[test]
     fn new_creates_rays(){
@@ -62,6 +69,28 @@ mod tests {
         assert!( r.position(2.5).equals(Tuple::point( Number::from(4.5),
                                                       Number::from(3),
                                                       Number::from(4))) );
+
+    }
+
+    #[test]
+    fn translating_a_ray(){
+        let o = Tuple::point( Number::from(1),
+                              Number::from(2),
+                              Number::from(3));
+        let d = Tuple::vector( Number::from(0),
+                               Number::from(1),
+                               Number::from(0));
+        let r = Ray::new(o, d);
+
+        let m = translation(3.0, 4.0, 5.0);
+        let r2 = r.transform(m);
+
+        assert!( r2.origin.equals(Tuple::point( Number::from(4),
+                                                Number::from(6),
+                                                Number::from(8))) );
+        assert!( r2.direction.equals(Tuple::vector( Number::from(0),
+                                                    Number::from(1),
+                                                    Number::from(0))) );
 
     }
 }
