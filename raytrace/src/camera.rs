a8ab308ae79ba9d7478e97634368d0c5bcd00980
josh-reynolds <josh.reynolds@frontier.com>
2024-07-01 13:52:02 -0700

    Adding tests and implementation for Camera.pixel_size field

--- raytrace/src/camera.rs
+++ raytrace/src/camera.rs
@@ -6,13 +6,26 @@ pub struct Camera {
     pub vsize: i32,
     pub field_of_view: f64,
     pub transform: Matrix,
+    pub pixel_size: f64,
 }
 
 pub fn camera(hsize: i32, vsize: i32, field_of_view: f64) -> Camera {
+    let half_view = (field_of_view / 2.0).tan();
+    let aspect = hsize as f64 / vsize as f64;
+    let mut half_height = half_view;
+    let mut half_width = half_view * aspect;
+
+    if aspect >= 1.0 {
+        half_width = half_view;
+        half_height = half_view / aspect;
+    }
+
     Camera{ hsize, 
             vsize, 
             field_of_view,
-            transform: identity(), }
+            transform: identity(),
+            pixel_size: (half_width * 2.0) / hsize as f64,
+    }
 }
 
 #[cfg(test)]
@@ -34,5 +47,17 @@ mod tests {
         assert!(equals(c.field_of_view, field_of_view));
         assert!(c.transform == identity());
 
+    }
+
+    #[test]
+    fn pixel_size_for_horizontal_canvas(){
+        let c = camera(200, 125, PI / 2.0);
+        assert!( equals( c.pixel_size, 0.01 ));
+    }
+
+    #[test]
+    fn pixel_size_for_vertical_canvas(){
+        let c = camera(125, 200, PI / 2.0);
+        assert!( equals( c.pixel_size, 0.01 ));
     }
 }
