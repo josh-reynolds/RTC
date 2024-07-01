b2a5ce2ab49130b6eed00152d60484d8d7374926
josh-reynolds <josh.reynolds@frontier.com>
2024-07-01 15:40:42 -0700

    Very basic render using new Camera and World features

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -1,49 +1,20 @@
-use raytrace::tuple::point;
-use raytrace::canvas::canvas;
-use raytrace::color::color;
-use raytrace::spheres::sphere;
-use raytrace::rays::ray;
-use raytrace::materials::{material, lighting};
-use raytrace::lights::point_light;
+use raytrace::tuple::{point, vector};
+use std::f64::consts::PI;
+use raytrace::camera::camera;
+use raytrace::transform::view_transform;
+use raytrace::world::default_world;
 
 fn main() {
-    let ray_origin = point( 0.0, 0.0, -5.0);
-    let wall_z = 10.0;
-    let wall_size = 7.0;
-    let half = wall_size / 2.0;
+    let w = default_world();
+        
+    let mut c = camera(200, 200, PI / 2.0);
+    let from = point(0.0, 0.0, -5.0);
+    let to = point(0.0, 0.0, 0.0);
+    let up = vector(0.0, 1.0, 0.0);
+    c.transform = view_transform(from, to, up);
 
-    let canvas_pixels = 100;
-    let pixel_size = wall_size / canvas_pixels as f64;
-    let mut c = canvas(canvas_pixels, canvas_pixels);
+    let image = c.render(w);
 
-    let mut shape = sphere();
-    shape.material = material();
-    shape.material.color = color(1.0, 0.2, 1.0);
-
-    let light_pos = point(-10.0, 10.0, -10.0);
-    let light_color = color(1.0, 1.0, 1.0);
-    let light = point_light(light_pos, light_color);
-
-    for y in 0..(canvas_pixels-1){
-        let world_y = half - pixel_size * y as f64;
-
-        for x in 0..(canvas_pixels-1){
-            let world_x = -half + pixel_size * x as f64;
-            let position = point(world_x, world_y, wall_z);
-            let r = ray(ray_origin, (position - ray_origin).normal());
-            let xs = shape.intersect(r);
-
-            if xs.len() > 0 {
-                let hit = xs[0];
-                let p = r.position( hit.t );
-                let normal = hit.object.normal_at( p );
-                let eye = -r.direction;
-                let col = lighting(hit.object.material, &light, p, eye, normal);
-                c.write_pixel(x, y, col);
-            }
-        }
-    }
-    
-    let _ = c.to_ppm("phong_render.ppm");
+    let _ = image.to_ppm("world_render.ppm");
 }
 
