3f251c41035766b0a9beb92e0fdc63988122a84b
josh-reynolds <josh.reynolds@frontier.com>
2024-07-01 14:42:24 -0700

    Adding tests and implementation for Camera.ray_for_pixel()

--- raytrace/src/camera.rs
+++ raytrace/src/camera.rs
@@ -1,4 +1,6 @@
 use crate::matrix::{Matrix, identity};
+use crate::rays::{Ray, ray};
+use crate::tuple::{point, origin};
 
 #[derive(Debug)]
 pub struct Camera {
@@ -7,6 +9,24 @@ pub struct Camera {
     pub field_of_view: f64,
     pub transform: Matrix,
     pub pixel_size: f64,
+    pub half_width: f64,
+    pub half_height: f64,
+}
+
+impl Camera {
+    pub fn ray_for_pixel(&self, px: i32, py: i32) -> Ray {
+        let xoffset = (px as f64 + 0.5) * self.pixel_size;
+        let yoffset = (py as f64 + 0.5) * self.pixel_size;
+
+        let world_x = self.half_width - xoffset;
+        let world_y = self.half_height - yoffset;
+
+        let pixel = self.transform.inverse().multup( &point(world_x, world_y, -1.0) );
+        let origin = self.transform.inverse().multup( &origin() );
+        let direction = (pixel - origin).normal();
+
+        ray( origin, direction )
+    }
 }
 
 pub fn camera(hsize: i32, vsize: i32, field_of_view: f64) -> Camera {
@@ -25,15 +45,19 @@ pub fn camera(hsize: i32, vsize: i32, field_of_view: f64) -> Camera {
             field_of_view,
             transform: identity(),
             pixel_size: (half_width * 2.0) / hsize as f64,
+            half_width,
+            half_height,
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::f64::consts::PI;
+    use std::f64::consts::{PI, SQRT_2};
     use crate::camera::camera;
     use crate::matrix::identity;
     use crate::equals::equals;
+    use crate::tuple::{point, vector};
+    use crate::transform::{rotation_y, translation};
 
     #[test]
     fn constructing_a_camera(){
@@ -59,5 +83,30 @@ mod tests {
     fn pixel_size_for_vertical_canvas(){
         let c = camera(125, 200, PI / 2.0);
         assert!( equals( c.pixel_size, 0.01 ));
+    }
+
+    #[test]
+    fn ray_through_center_of_canvas(){
+        let c = camera(201, 101, PI / 2.0);
+        let r = c.ray_for_pixel(100, 50);
+        assert!( r.origin.equals( point(0.0, 0.0, 0.0) ));
+        assert!( r.direction.equals( vector(0.0, 0.0, -1.0) ));
+    }
+
+    #[test]
+    fn ray_through_corner_of_canvas(){
+        let c = camera(201, 101, PI / 2.0);
+        let r = c.ray_for_pixel(0, 0);
+        assert!( r.origin.equals( point(0.0, 0.0, 0.0) ));
+        assert!( r.direction.equals( vector(0.66519, 0.33259, -0.66851) ));
+    }
+
+    #[test]
+    fn ray_when_camera_is_transformed(){
+        let mut c = camera(201, 101, PI / 2.0);
+        c.transform = rotation_y(PI / 4.0).mult( &translation(0.0, -2.0, 5.0) );
+        let r = c.ray_for_pixel(100, 50);
+        assert!( r.origin.equals( point(0.0, 2.0, -5.0) ));
+        assert!( r.direction.equals( vector(SQRT_2 / 2.0, 0.0, -SQRT_2 / 2.0) ));
     }
 }
