b6c0fb641d6f0f9e7a0296965cba882bc506df54
josh-reynolds <josh.reynolds@frontier.com>
2024-09-04 10:25:28 -0700

    Another test render

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -1,7 +1,7 @@
 use raytrace::tuple::{point, vector};
 use std::f64::consts::PI;
 use raytrace::camera::camera;
-use raytrace::transform::{view_transform, translation, scaling};
+use raytrace::transform::{view_transform, translation, scaling, rotation_x};
 use raytrace::world::world;
 use raytrace::spheres::sphere;
 use raytrace::planes::plane;
@@ -9,6 +9,7 @@ use raytrace::shapes::Shape;
 use raytrace::color::color;
 use raytrace::materials::material;
 use raytrace::lights::point_light;
+use raytrace::patterns::Pattern;
 use raytrace::stripes::stripe_pattern;
 use std::time::Instant;
 
@@ -25,11 +26,19 @@ fn main() {
     floor.set_material( mat );
     w.add_object(Box::new(floor));
 
+    let mut wall = plane();
+    wall.set_transform(rotation_x(PI/2.0).mult(
+                        &translation(0.0, 15.0, 0.0)));
+    let mut mat = material();
+    mat.color = color(0.9, 0.8, 0.7);
+    wall.set_material(mat);
+    w.add_object(Box::new(wall));
+
     let mut middle = sphere();
     middle.set_transform( translation(-0.5, 1.0, 0.5) );
     let mut mat = material();
     mat.reflective = 0.8;
-    mat.color = color(0.0, 0.0, 0.0);
+    mat.color = color(0.1, 0.1, 0.1);
     mat.diffuse = 0.7;
     mat.specular = 0.3;
     middle.set_material( mat );
@@ -39,7 +48,7 @@ fn main() {
     right.set_transform( translation(1.5, 0.5, -0.5).mult(
                           &scaling(0.5, 0.5, 0.5)));
     let mut mat = material();
-    mat.color = color(0.0, 0.0, 0.0);
+    mat.color = color(0.0, 0.0, 0.4);
     mat.diffuse = 0.1;
     mat.specular = 1.0;
     mat.shininess = 300.0;
@@ -53,7 +62,8 @@ fn main() {
     left.set_transform( translation(-1.5, 0.33, -0.75).mult(
                           &scaling(0.33, 0.33, 0.33)));
     let mut mat = material();
-    let p4 = stripe_pattern(color(1.0, 0.0, 0.0), color(0.0, 1.0, 0.0));
+    let mut p4 = stripe_pattern(color(1.0, 0.0, 0.0), color(1.0, 1.0, 0.0));
+    p4.set_pattern_transform(scaling(0.5, 0.5, 0.5));
     let current = w.add_pattern( Box::new(p4) );
     mat.pattern = Some(current);
     mat.color = color(0.1, 0.1, 0.9);
@@ -70,7 +80,7 @@ fn main() {
 
     let image = c.render(w);
 
-    let _ = image.to_ppm("transparency_final.ppm");
+    let _ = image.to_ppm("transparency_final_2.ppm");
 
     let elapsed = now.elapsed();
     println!("Size: {} x {}", c.hsize, c.vsize);
