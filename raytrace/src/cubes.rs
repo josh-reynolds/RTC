f9405b7bd2af724a461658323975dd92050e106e
josh-reynolds <josh.reynolds@frontier.com>
2024-09-05 13:39:29 -0700

    Implementing Cube.intersect(), with helper method and corresponding test

--- raytrace/src/cubes.rs
+++ raytrace/src/cubes.rs
@@ -4,6 +4,8 @@ use crate::intersections::{Intersection, intersection, intersections};
 use crate::matrix::Matrix;
 use crate::shapes::{Base, Shape, shape};
 use crate::materials::Material;
+use crate::equals::EPSILON;
+use std::f64::INFINITY;
 
 pub struct Cube {
     supe: Base,
@@ -31,26 +33,19 @@ impl Shape for Cube {
     }
 
     fn intersect(&self, r: Ray) -> Vec<Intersection> {
-        let r2 = self.saved_ray(r);
-        let sphere_to_ray = r2.origin - origin();
+        let xvals = self.check_axis(r.origin.x, r.direction.x);
+        let yvals = self.check_axis(r.origin.y, r.direction.y);
+        let zvals = self.check_axis(r.origin.z, r.direction.z);
 
-        let a = r2.direction.dot(&r2.direction);
-        let b = 2.0 * ( r2.direction.dot(&sphere_to_ray) );
-        let c = (sphere_to_ray.dot(&sphere_to_ray)) - 1.0;
+        let mins = [xvals.0, yvals.0, zvals.0];
+        let maxs = [xvals.1, yvals.1, zvals.1];
+        
+        let tmin = mins.iter().max_by(|a,b| a.total_cmp(b)).unwrap();
+        let tmax = maxs.iter().min_by(|a,b| a.total_cmp(b)).unwrap();
 
-        let discriminant = b.powf(2.0) - 4.0 * a * c;
-
-        if discriminant < 0.0 {
-            return vec!();
-        } else {
-            let t1 = (-b - discriminant.sqrt()) / ( 2.0 * a);
-            let i1 = intersection(t1, self.get_index());
-
-            let t2 = (-b + discriminant.sqrt()) / ( 2.0 * a);
-            let i2 = intersection(t2, self.get_index());
-
-            return intersections(&[i1,i2]);
-        }
+        let i1 = intersection(*tmin, self.get_index());
+        let i2 = intersection(*tmax, self.get_index());
+        return intersections(&[i1, i2]);
     }
 
     fn get_index(&self) -> usize {
@@ -59,6 +54,32 @@ impl Shape for Cube {
 
     fn set_index(&mut self, index: usize){
         self.supe.set_index(index);
+    }
+}
+
+impl Cube {
+    fn check_axis(&self, o: f64, d: f64) -> (f64, f64){
+        let tmin_numerator = -1.0 - o;
+        let tmax_numerator =  1.0 - o;
+
+        let mut tmin;
+        let mut tmax;
+
+        if d.abs() > EPSILON {
+            tmin = tmin_numerator / d;
+            tmax = tmax_numerator / d;
+        } else {
+            tmin = tmin_numerator * INFINITY;
+            tmax = tmax_numerator * INFINITY;
+        }
+
+        if tmin > tmax {
+            let temp = tmin;
+            tmin = tmax;
+            tmax = temp;
+        }
+
+        (tmin, tmax)
     }
 }
 
@@ -71,9 +92,61 @@ pub fn cube() -> Cube {
 #[cfg(test)]
 mod tests {
     use crate::cubes::cube;
+    use crate::shapes::Shape;
+    use crate::tuple::{point, vector};
+    use crate::rays::ray;
 
     #[test]
     fn a_ray_intersects_a_cube(){
-        let _c = cube();
+        let c = cube();
+
+        // positive x
+        let r = ray(point(5.0, 0.5, 0.0), vector(-1.0, 0.0, 0.0), 0);
+        let xs = c.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+
+        // negative x
+        let r = ray(point(-5.0, 0.5, 0.0), vector(1.0, 0.0, 0.0), 0);
+        let xs = c.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+
+        // positive y
+        let r = ray(point(0.5, 5.0, 0.0), vector(0.0, -1.0, 0.0), 0);
+        let xs = c.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+        
+        // negative y
+        let r = ray(point(0.5, -5.0, 0.0), vector(0.0, 1.0, 0.0), 0);
+        let xs = c.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+        
+        // positive z
+        let r = ray(point(0.5, 0.0, 5.0), vector(0.0, 0.0, -1.0), 0);
+        let xs = c.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+        
+        // negative z
+        let r = ray(point(0.5, 0.0, -5.0), vector(0.0, 0.0, 1.0), 0);
+        let xs = c.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+
+        // inside
+        let r = ray(point(0.0, 0.5, 0.0), vector(0.0, 0.0, 1.0), 0);
+        let xs = c.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -1.0);
+        assert_eq!(xs[1].t, 1.0);
     }
 }
