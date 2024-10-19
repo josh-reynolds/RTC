189924a01a79c612bc9ae8b2ccbf8d0d2c302149
josh-reynolds <josh.reynolds@frontier.com>
2024-10-18 17:06:06 -0700

    Adding ctors and corresponding tests - also sketching out rest of pattern interface

--- poly/src/composite.rs
+++ poly/src/composite.rs
@@ -1,10 +1,32 @@
 use crate::component::Component;
 
 pub struct Composite {
+    value: usize,
+    //children: Vec<Box<dyn Component>>
 }
 
 impl Component for Composite {
     fn operation(&self) -> usize{
-        0
+        self.value
+        //for all g in children: g.operation()
+    }
+    //fn add(&self, Component: c);
+    //fn remove(&self, Component: c);
+    //fn get_child(&self, usize: i);
+}
+
+pub fn composite() -> Composite {
+    Composite{ value: 0 }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::composite::composite;
+    use crate::component::Component;
+
+    #[test]
+    fn constructing_a_composite(){
+        let c = composite();
+        assert!(c.operation() == 0);
     }
 }
--- poly/src/leaf.rs
+++ poly/src/leaf.rs
@@ -1,10 +1,30 @@
 use crate::component::Component;
 
 pub struct Leaf {
+    value: usize,
 }
 
 impl Component for Leaf {
     fn operation(&self) -> usize{
-        0
+        self.value
+    }
+    //fn add(&self, Component: c);
+    //fn remove(&self, Component: c);
+    //fn get_child(&self, usize: i);
+}
+
+pub fn leaf() -> Leaf {
+    Leaf{ value: 1 }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::leaf::leaf;
+    use crate::component::Component;
+
+    #[test]
+    fn constructing_a_leaf(){
+        let l = leaf();
+        assert!(l.operation() == 1);
     }
 }
