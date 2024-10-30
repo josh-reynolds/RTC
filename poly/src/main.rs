30abb6e712201ebbec0c97805b1ddd265c439a6a
josh-reynolds <josh.reynolds@frontier.com>
2024-10-30 13:42:14 -0700

    Adjusting main module to use Coordinate index values

--- poly/src/main.rs
+++ poly/src/main.rs
@@ -4,18 +4,22 @@ fn main() {
     let l = leaf();
     let mut c1 = composite();
     let mut c2 = composite();
-    c1.add(l);
-    c2.add(c1);
+    
+    let first = c1.add(l).unwrap();
+    let second = c2.add(c1).unwrap();
 
     println!("{:?}", c2);
 
-    let grp = c2.get_child(0).unwrap();
+    let grp = c2.get_child(first.index).unwrap();
     println!("{:?}", grp);
 
-    let ch = c2.get_child(0)
+    let ch = c2.get_child(first.index)
                .expect("VALID INDEX")
-               .get_child(0)
+               .get_child(second.index)
                .unwrap();
 
     println!("{:?}", ch);
+
+    let r = &ch;
+    println!("{:?}", *r);
 }
