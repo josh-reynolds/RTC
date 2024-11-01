8c75641c1b958878e8b9f320e96bbdf3481c29c7
josh-reynolds <josh.reynolds@frontier.com>
2024-10-31 20:25:16 -0700

    Adding parent field to Coordinate

--- poly/src/coordinate.rs
+++ poly/src/coordinate.rs
@@ -2,11 +2,13 @@
 #[derive(Debug,Clone)]
 pub struct Coordinate {
     pub index: usize,
+    pub parent: usize,
 }
 
 pub fn coordinate(i: usize) -> Coordinate {
     Coordinate {
         index: i,
+        parent: 0,
     }
 }
 
@@ -18,6 +20,7 @@ mod tests {
     fn constructing_a_coordinate(){
         let c = coordinate(0);
         assert!(c.index == 0);
+        assert!(c.parent == 0);
     }
 }
 
