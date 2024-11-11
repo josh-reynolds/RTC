ae8b28dc5ceb71157ae81583d4326e0f43e3557d
josh-reynolds <josh.reynolds@frontier.com>
2024-11-11 13:22:45 -0800

    Adding point() function, test for same, and implementing equality for Tuple

--- python/tuple.py
+++ python/tuple.py
@@ -14,6 +14,18 @@ class Tuple():
     def isVector(self):
         return self.w == 0.0
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+def point(x, y, z):
+    return Tuple(x, y, z, 1)
+
 
 class TupleTestCase(unittest.TestCase):
     def test_tuple_with_w_1_is_point(self):
@@ -22,8 +34,8 @@ class TupleTestCase(unittest.TestCase):
         self.assertEqual(a.y, -4.2)
         self.assertEqual(a.z,  3.1)
         self.assertEqual(a.w,  1.0)
-        self.assertEqual(a.isPoint(), True)
-        self.assertEqual(a.isVector(), False)
+        self.assertTrue(a.isPoint())
+        self.assertFalse(a.isVector())
 
     def test_tuple_with_w_0_is_vector(self):
         a = Tuple(4.3, -4.2, 3.1, 0.0)
@@ -31,6 +43,12 @@ class TupleTestCase(unittest.TestCase):
         self.assertEqual(a.y, -4.2)
         self.assertEqual(a.z,  3.1)
         self.assertEqual(a.w,  0.0)
-        self.assertEqual(a.isPoint(), False)
-        self.assertEqual(a.isVector(), True)
+        self.assertFalse(a.isPoint())
+        self.assertTrue(a.isVector())
+
+    def test_point_creates_tuples_with_w_1(self):
+        a = point(4, -4, 3)
+        self.assertTrue(a.isPoint())
+        self.assertFalse(a.isVector())
+        self.assertEqual(a, Tuple(4, -4, 3, 1))
 
