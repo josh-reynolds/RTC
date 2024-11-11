9b94509e84e640aa80ff89c13cc138a1945b36f5
josh-reynolds <josh.reynolds@frontier.com>
2024-11-11 13:08:39 -0800

    Adding test for Tuple.isVector()

--- python/tuple.py
+++ python/tuple.py
@@ -7,8 +7,10 @@ class Tuple():
         self.y = y
         self.z = z
         self.w = w
+    
     def isPoint(self):
         return self.w == 1.0
+
     def isVector(self):
         return self.w == 0.0
 
@@ -22,4 +24,13 @@ class TupleTestCase(unittest.TestCase):
         self.assertEqual(a.w,  1.0)
         self.assertEqual(a.isPoint(), True)
         self.assertEqual(a.isVector(), False)
+
+    def test_tuple_with_w_0_is_vector(self):
+        a = Tuple(4.3, -4.2, 3.1, 0.0)
+        self.assertEqual(a.x,  4.3)
+        self.assertEqual(a.y, -4.2)
+        self.assertEqual(a.z,  3.1)
+        self.assertEqual(a.w,  0.0)
+        self.assertEqual(a.isPoint(), False)
+        self.assertEqual(a.isVector(), True)
 
