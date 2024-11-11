07b05517c83f9e735c8b965b5f349b6d1a7c7bc7
josh-reynolds <josh.reynolds@frontier.com>
2024-11-11 13:06:22 -0800

    Basic Tuple class and first test

--- python/tuple.py
+++ python/tuple.py
@@ -1,4 +1,25 @@
+# to run tests: python -m unittest -v tuple
+import unittest
+
+class Tuple():
+    def __init__(self, x, y, z, w):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+    def isPoint(self):
+        return self.w == 1.0
+    def isVector(self):
+        return self.w == 0.0
 
 
+class TupleTestCase(unittest.TestCase):
+    def test_tuple_with_w_1_is_point(self):
+        a = Tuple(4.3, -4.2, 3.1, 1.0)
+        self.assertEqual(a.x,  4.3)
+        self.assertEqual(a.y, -4.2)
+        self.assertEqual(a.z,  3.1)
+        self.assertEqual(a.w,  1.0)
+        self.assertEqual(a.isPoint(), True)
+        self.assertEqual(a.isVector(), False)
 
-print("Hello!")
