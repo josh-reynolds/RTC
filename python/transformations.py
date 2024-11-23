f92951ca8c3199b7419073874dbc9c84c72af8a6
josh-reynolds <josh.reynolds@frontier.com>
2024-11-22 17:04:02 -0800

    Additional tests to triangulate implementation of translation()

--- python/transformations.py
+++ python/transformations.py
@@ -1,7 +1,7 @@
 # to run tests: python -m unittest -v transformations
 
 import unittest
-from tuple import point
+from tuple import point, vector
 from matrix import identity
 
 def translation(dx, dy, dz):
@@ -14,6 +14,21 @@ class TransformationsTestCase(unittest.TestCase):
         p = point(-3, 4, 5)
 
         self.assertEqual(transform * p, point(2, 1, 7))
+
+    def test_multiplying_by_inverse_of_a_translation_matrix(self):
+        transform = translation(5, -3, 2)
+        inv = transform.inverse()
+        p = point(-3, 4, 5)
+
+        self.assertEqual(inv * p, point(-8, 7, 3))
+
+    def test_translation_does_not_affect_vectors(self):
+        transform = translation(5, -3, 2)
+        v = vector(-3, 4, 5)
+
+        self.assertEqual(transform * v, v)
+
+
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
