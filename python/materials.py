049c7049ded5540e83422d6e8c424144fe5eeea3
josh-reynolds <josh.reynolds@frontier.com>
2024-11-30 18:15:39 -0800

    Adding lighting() function with naive implementation and initial test

--- python/materials.py
+++ python/materials.py
@@ -3,14 +3,16 @@
 import unittest
 from color import color
 from utils import flequal
+from lights import point_light
+from tuple import point, vector
 
 class Material:
     def __init__(self):
         self.color = color(1, 1, 1)
-        self.ambient = 0.1
-        self.diffuse = 0.9
-        self.specular = 0.9
-        self.shininess = 200.0
+        self.ambient = 0.1        # range 0 - 1
+        self.diffuse = 0.9        # range 0 - 1
+        self.specular = 0.9       # range 0 - 1
+        self.shininess = 200.0    # range 10 - 200
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -25,6 +27,9 @@ class Material:
 def material():
     return Material()
 
+def lighting(material, light, position, eyev, normalv):
+    return color(1.9, 1.9, 1.9)
+
 class MaterialTestCase(unittest.TestCase):
     def test_the_default_material(self):
         m = material()
@@ -34,6 +39,17 @@ class MaterialTestCase(unittest.TestCase):
         self.assertEqual(m.diffuse, 0.9)
         self.assertEqual(m.specular, 0.9)
         self.assertEqual(m.shininess, 200.0)
+
+    def test_lighting_with_eye_between_light_and_surface(self):
+        m = material()
+        light = point_light(point(0, 0, -10), color(1, 1, 1))
+        position = point(0, 0, 0)
+        eyev = vector(0, 0, -1)
+        normalv = vector(0, 0, -1)
+        
+        result = lighting(m, light, position, eyev, normalv)
+
+        self.assertEqual(result, color(1.9, 1.9, 1.9))
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
