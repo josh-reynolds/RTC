7be231bd24e26d347f71ea354838a010b5d389ef
josh-reynolds <josh.reynolds@frontier.com>
2024-12-04 17:37:00 -0800

    The world render project from the end of Chapter 7

--- python/main.py
+++ python/main.py
@@ -1,48 +1,69 @@
 import math
-from tuple import point
-from canvas import canvas
+from tuple import point, vector
 from color import color
 from spheres import sphere
-from rays import ray
-from intersections import hit
+from world import world
 from lights import point_light
-from materials import lighting
+from camera import camera
+from transformations import view_transform, translation, rotation_y, rotation_x, scaling
 
-ray_origin = point(0, 0, -5)
-wall_z = 10
-wall_size = 7.0
+floor = sphere()
+floor.transform = scaling(10, 0.01, 10)
+floor.material.color = color(1, 0.9, 0.9)
+floor.material.specular = 0
 
-canvas_pixels = 100
-pixel_size = wall_size / canvas_pixels
-half = wall_size / 2
-c = canvas(canvas_pixels, canvas_pixels)
+left_wall = sphere()
+left_wall.transform = (translation(0, 0, 5) * 
+                       rotation_y(-math.pi/4) * 
+                       rotation_x(math.pi/2) * 
+                       scaling(10, 0.01, 10))
+left_wall.material = floor.material
 
-shape = sphere()
-shape.material.color = color(1, 0.2, 1)
+right_wall = sphere()
+right_wall.transform = (translation(0, 0, 5) * 
+                        rotation_y(math.pi/4) * 
+                        rotation_x(math.pi/2) * 
+                        scaling(10, 0.01, 10))
+right_wall.material = floor.material
 
-light_position = point(-10, 10, -10)
-light_color = color(1, 1, 1)
-light = point_light(light_position, light_color)
+middle = sphere()
+middle.transform = translation(-0.5, 1, 0.5)
+middle.material.color = color(0.1, 1, 0.5)
+middle.material.diffuse = 0.7
+middle.material.specular = 0.3
 
-for y in range(canvas_pixels):
-    world_y = half - pixel_size * y
-    
-    for x in range(canvas_pixels):
-        world_x = -half + pixel_size * x
-        position = point(world_x, world_y, wall_z)
-        r = ray(ray_origin, (position - ray_origin).normalize())
-        xs = shape.intersect(r)
-        h = hit(xs)
+right = sphere()
+right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5)
+right.material.color = color(0.5, 1, 0.1)
+right.material.diffuse = 0.7
+right.material.specular = 0.3
 
-        if h:
-            pt = r.position(h.t)
-            normal = h.object.normal_at(pt)
-            eye = -r.direction
-            col = lighting(h.object.material, light, pt, eye, normal)
-            c.write_pixel(x, y, col)
+left = sphere()
+left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
+left.material.color = color(1, 0.8, 0.1)
+left.material.diffuse = 0.7
+left.material.specular = 0.3
 
-f = open("phong_sphere.ppm", "w")
-lines = c.to_ppm()
+w = world()
+
+w.objects.append(floor)
+w.objects.append(left_wall)
+w.objects.append(right_wall)
+w.objects.append(middle)
+w.objects.append(right)
+w.objects.append(left)
+
+w.light = point_light(point(-10, 10, -10), color(1, 1, 1))
+
+cam = camera(100, 50, math.pi/3)
+cam.transform = view_transform(point(0, 1.5, -5),
+                               point(0, 1, 0),
+                               vector(0, 1, 0))
+
+image = cam.render(w)
+
+f = open("world_render.ppm", "w")
+lines = image.to_ppm()
 for line in lines:
     f.write(line + "\n")
 f.close()
