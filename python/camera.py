841d618c64c394cc99aaac4908fa07585646f47c
josh-reynolds <josh.reynolds@frontier.com>
2024-12-04 17:00:50 -0800

    Adding Camera.render() and corresponding test

--- python/camera.py
+++ python/camera.py
@@ -6,7 +6,10 @@ from matrix import identity
 from utils import flequal
 from tuple import point, vector
 from rays import ray
-from transformations import rotation_y, translation
+from transformations import rotation_y, translation, view_transform
+from world import default_world
+from canvas import canvas
+from color import color
 
 class Camera:
     def __init__(self, hsize, vsize, field_of_view):
@@ -39,6 +42,17 @@ class Camera:
         direction = (pixel - origin).normalize()
 
         return ray(origin, direction)
+    
+    def render(self, world):
+        image = canvas(self.hsize, self.vsize)
+
+        for y in range(self.vsize):
+            for x in range (self.hsize):
+                r = self.ray_for_pixel(x, y)
+                col = world.color_at(r)
+                image.write_pixel(x, y, col)
+
+        return image
 
 def camera(hsize, vsize, field_of_view):
     return Camera(hsize, vsize, field_of_view)
@@ -90,6 +104,19 @@ class CameraTestCase(unittest.TestCase):
 
         self.assertEqual(r.origin, point(0, 2, -5))
         self.assertEqual(r.direction, vector(math.sqrt(2)/2, 0, -math.sqrt(2)/2))
+
+    def test_rendering_a_world_with_a_camera(self):
+        w = default_world()
+        c = camera(11, 11, math.pi/2)
+
+        frm = point(0, 0, -5)
+        to = point(0, 0, 0)
+        up = vector(0, 1, 0)
+        c.transform = view_transform(frm, to, up)
+
+        image = c.render(w)
+
+        self.assertEqual(image.pixel_at(5, 5), color(0.38066, 0.47583, 0.2855))
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
