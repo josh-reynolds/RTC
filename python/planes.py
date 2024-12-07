0b768199a8c1bfb259379f1fe5c4a8662c281971
josh-reynolds <josh.reynolds@frontier.com>
2024-12-07 12:59:10 -0800

    Adding Plane.local_intersect() with naive implementation and initial test

--- python/planes.py
+++ python/planes.py
@@ -2,7 +2,7 @@
 
 import unittest
 #import math
-#from rays import ray
+from rays import ray
 from tuple import point, vector
 #import intersections
 #from matrix import identity
@@ -15,28 +15,11 @@ class Plane(Shape):
         Shape.__init__(self)
 
     def local_intersect(self, r):
-        pass
-        #result = []
-        #sphere_to_ray = r.origin - point(0, 0, 0)
-#
-        #a = r.direction.dot(r.direction)
-        #b = 2 * r.direction.dot(sphere_to_ray)
-        #c = sphere_to_ray.dot(sphere_to_ray) - 1
-#
-        #discriminant = (b ** 2) - (4 * a * c)
-#
-        #if discriminant >= 0:
-            #dsqrt = math.sqrt(discriminant)
-            #t1 = (-b - dsqrt) / (2 * a)
-            #t2 = (-b + dsqrt) / (2 * a)
-            #result.append(intersections.intersection(t1, self))
-            #result.append(intersections.intersection(t2, self))
-#
-        #return result
+        result = []
+        return result
 
     def local_normal_at(self, pt):
         return vector(0, 1, 0)
-        #return pt - point(0, 0, 0)
 
 def plane():
     return Plane()
@@ -52,16 +35,14 @@ class PlaneTestCase(unittest.TestCase):
         self.assertEqual(n2, vector(0, 1, 0))
         self.assertEqual(n3, vector(0, 1, 0))
 
-    #def test_a_ray_intersects_a_sphere_at_two_points(self):
-        #r = ray(point(0, 0, -5), vector(0, 0, 1))
-        #s = sphere()
-#
-        #xs = s.intersect(r)
-#
-        #self.assertEqual(len(xs), 2)
-        #self.assertEqual(xs[0].t, 4.0)
-        #self.assertEqual(xs[1].t, 6.0)
-#
+    def test_intersect_with_ray_parallel_to_plane(self):
+        p = plane()
+        r = ray(point(0, 10, 0), vector(0, 0, 1))
+
+        xs = p.intersect(r)
+
+        self.assertEqual(len(xs), 0)
+
     #def test_sphere_returns_unique_instances(self):
         #s1 = sphere()
         #s2 = sphere()
@@ -206,11 +187,11 @@ class PlaneTestCase(unittest.TestCase):
         #s.material = m
 #
         #self.assertEqual(s.material, m)
-#
-    #def test_a_sphere_is_a_shape(self):
-        #s = sphere()
-#
-        #self.assertTrue(isinstance(s, Shape))
+
+    def test_a_sphere_is_a_shape(self):
+        p = plane()
+
+        self.assertTrue(isinstance(p, Shape))
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
