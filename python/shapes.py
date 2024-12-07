1285d6f43eedf6878be0f716b457efb3bc96a8bd
josh-reynolds <josh.reynolds@frontier.com>
2024-12-06 16:02:39 -0800

    Adding Shape.intersect() and Shape.local_intersect() with corresponding tests

--- python/shapes.py
+++ python/shapes.py
@@ -2,11 +2,11 @@
 
 import unittest
 #import math
-#from rays import ray
-#from tuple import point, vector
+from rays import ray
+from tuple import point, vector
 #import intersections
 from matrix import identity
-from transformations import translation
+from transformations import translation, scaling
 from materials import material
 
 class Shape:
@@ -21,25 +21,12 @@ class Shape:
         #else:
             #return False
 #
-    #def intersect(self, r):
-        #result = []
-        #r2 = r.transform(self.transform.inverse())
-        #sphere_to_ray = r2.origin - point(0, 0, 0)
-#
-        #a = r2.direction.dot(r2.direction)
-        #b = 2 * r2.direction.dot(sphere_to_ray)
-        #c = sphere_to_ray.dot(sphere_to_ray) - 1
-#
-        #discriminant = (b ** 2) - (4 * a * c)
-#
-        #if discriminant >= 0:
-            #dsqrt = math.sqrt(discriminant)
-            #t1 = (-b - dsqrt) / (2 * a)
-            #t2 = (-b + dsqrt) / (2 * a)
-            #result.append(intersections.intersection(t1, self))
-            #result.append(intersections.intersection(t2, self))
-#
-        #return result
+    def intersect(self, r):
+        local_ray = r.transform(self.transform.inverse())
+        return self.local_intersect(local_ray)
+
+    def local_intersect(self, r):
+        self.saved_ray = r
     
     def set_transform(self, t):
         self.transform = t
@@ -80,6 +67,26 @@ class ShapeTestCase(unittest.TestCase):
         s.material = m
 
         self.assertEqual(s.material, m)
+
+    def test_intersecting_a_scaled_shape_with_a_ray(self):
+        s = test_shape()
+        s.set_transform(scaling(2, 2, 2))
+        r = ray(point(0, 0, -5), vector(0, 0, 1))
+
+        xs = s.intersect(r)
+
+        self.assertEqual(s.saved_ray.origin, point(0, 0, -2.5))
+        self.assertEqual(s.saved_ray.direction, vector(0, 0, 0.5))
+
+    def test_intersecting_a_translated_shape_with_a_ray(self):
+        s = test_shape()
+        s.set_transform(translation(5, 0, 0))
+        r = ray(point(0, 0, -5), vector(0, 0, 1))
+
+        xs = s.intersect(r)
+
+        self.assertEqual(s.saved_ray.origin, point(-5, 0, -5))
+        self.assertEqual(s.saved_ray.direction, vector(0, 0, 1))
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
