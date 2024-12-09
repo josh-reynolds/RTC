fc4cb66c52bc5d955a4f5a3e8b74cd5d94e47c15
josh-reynolds <josh.reynolds@frontier.com>
2024-12-08 16:45:59 -0800

    Adding naive implementation of Stripe.stripe_at() and initial test

--- python/patterns.py
+++ python/patterns.py
@@ -2,11 +2,15 @@
 
 import unittest
 from color import color
+from tuple import point
 
 class Stripe():
     def __init__(self, color1, color2):
         self.a = color1
         self.b = color2
+
+    def stripe_at(self, pt):
+        return WHITE
 
 def stripe_pattern(color1, color2):
     return Stripe(color1, color2)
@@ -24,6 +28,13 @@ class PatternsTestCase(unittest.TestCase):
 
         self.assertEqual(pattern.a, WHITE)
         self.assertEqual(pattern.b, BLACK)
+
+    def test_a_stripe_pattern_is_constant_in_y(self):
+        pattern = stripe_pattern(WHITE, BLACK)
+
+        self.assertEqual(pattern.stripe_at(point(0, 0, 0)), WHITE)
+        self.assertEqual(pattern.stripe_at(point(0, 1, 0)), WHITE)
+        self.assertEqual(pattern.stripe_at(point(0, 2, 0)), WHITE)
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
