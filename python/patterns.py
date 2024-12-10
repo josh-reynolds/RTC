b68a3cf75498b35a901fd24464d5a242cd85c984
josh-reynolds <josh.reynolds@frontier.com>
2024-12-09 16:10:22 -0800

    Proper implementation of Stripe.stripe_at_object()

--- python/patterns.py
+++ python/patterns.py
@@ -5,13 +5,14 @@ import math
 from color import color
 from tuple import point
 import spheres
-from transformations import scaling
+from transformations import scaling, translation
+from matrix import identity
 
 class Stripe():
     def __init__(self, color1, color2):
         self.a = color1
         self.b = color2
-        self.transform = None
+        self.transform = identity()
 
     def stripe_at(self, pt):
         if math.floor(pt.x) % 2 == 0:
@@ -19,8 +20,11 @@ class Stripe():
         else:
             return self.b
 
-    def stripe_at_object(self, obj, pt):
-        return WHITE
+    def stripe_at_object(self, obj, world_pt):
+        obj_pt = obj.transform.inverse() *  world_pt
+        pat_pt = self.transform.inverse() * obj_pt
+
+        return self.stripe_at(pat_pt)
 
     def set_transform(self, transform):
         self.transform = transform
@@ -81,6 +85,16 @@ class PatternsTestCase(unittest.TestCase):
         pattern.set_transform(scaling(2, 2, 2))
 
         c = pattern.stripe_at_object(obj, point(1.5, 0, 0))
+
+        self.assertEqual(c, WHITE)
+
+    def test_stripes_with_both_pattern_and_object_transformations(self):
+        obj = spheres.sphere()
+        obj.set_transform(scaling(2, 2, 2))
+        pattern = stripe_pattern(WHITE, BLACK)
+        pattern.set_transform(translation(0.5, 0, 0))
+
+        c = pattern.stripe_at_object(obj, point(2.5, 0, 0))
 
         self.assertEqual(c, WHITE)
 
