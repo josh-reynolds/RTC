988ee20446c32afe5cb07e84dd95b587aaa686e9
josh-reynolds <josh.reynolds@frontier.com>
2024-12-12 11:06:52 -0800

    Pattern render project from the end of Chapter 10

--- python/main.py
+++ python/main.py
@@ -6,14 +6,17 @@ from spheres import sphere
 from worlds import world
 from lights import point_light
 from cameras import camera
-from transformations import view_transform, translation, rotation_z, scaling
+from transformations import view_transform, translation, rotation_x, rotation_z, scaling
 from planes import plane
-from patterns import stripe_pattern
+from stripes import stripe_pattern
+from gradients import gradient_pattern
+from rings import ring_pattern
+from checkers import checker_pattern
 
 start_time = datetime.now()
 
 floor = plane()
-floor.material.pattern = stripe_pattern(color(0.7, 0, 0.7), color(0, 0.7, 0))
+floor.material.pattern = checker_pattern(color(1, 0, 0), color(0, 0, 1))
 
 middle = sphere()
 middle.transform = translation(-0.5, 1, 0.5) * rotation_z(math.pi/4)
@@ -27,30 +30,32 @@ right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5)
 right.material.color = color(0.5, 1, 0.1)
 right.material.diffuse = 0.7
 right.material.specular = 0.3
+right.material.pattern = gradient_pattern(color(1, 0, 1), color(0, 1, 0))
+right.material.pattern.set_transform(translation(1, 0, 0) * scaling(2, 1, 1))
 
 left = sphere()
 left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
 left.material.color = color(1, 0.8, 0.1)
 left.material.diffuse = 0.7
 left.material.specular = 0.3
+left.material.pattern = ring_pattern(color(1, 1, 0), color(0, 0, 1))
+left.material.pattern.set_transform(scaling(0.1, 0.1, 0.1) * rotation_x(math.pi/2))
 
 w = world()
-
 w.objects.append(floor)
 w.objects.append(middle)
 w.objects.append(right)
 w.objects.append(left)
-
 w.light = point_light(point(-10, 10, -10), color(1, 1, 1))
 
-cam = camera(300, 150, math.pi/3)
+cam = camera(200, 100, math.pi/3)
 cam.transform = view_transform(point(0, 1.5, -5),
                                point(0, 1, 0),
                                vector(0, 1, 0))
 
 image = cam.render(w)
 
-f = open("./output/pattern_render_2.ppm", "w")
+f = open("./output/pattern_render_3.ppm", "w")
 lines = image.to_ppm()
 for line in lines:
     f.write(line + "\n")
