a3d701a6a37b2b14a5fe6c721877cd9ae982457a
josh-reynolds <josh.reynolds@frontier.com>
2024-12-11 16:43:23 -0800

    Removing redundant test from stripes

--- python/stripes.py
+++ python/stripes.py
@@ -25,10 +25,6 @@ def stripe_pattern(color1, color2):
     return Stripe(color1, color2)
 
 class StripeTestCase(unittest.TestCase):
-    def test_color_constants(self):
-        self.assertEqual(BLACK, color(0, 0, 0))
-        self.assertEqual(WHITE, color(1, 1, 1))
-
     def test_creating_a_stripe_pattern(self):
         pattern = stripe_pattern(WHITE, BLACK)
 
