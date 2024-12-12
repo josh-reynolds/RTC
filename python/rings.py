d3df9dacbd8663ec9a38526d0ac909b4c6c34169
josh-reynolds <josh.reynolds@frontier.com>
2024-12-11 18:27:28 -0800

    Proper implementation of Ring.pattern_at() and corresponding test

--- python/rings.py
+++ python/rings.py
@@ -16,7 +16,10 @@ class Ring(patterns.Pattern):
         patterns.Pattern.__init__(self)
 
     def pattern_at(self, pt):
-        pass
+        if math.floor(math.sqrt(pt.x ** 2 + pt.z ** 2)) % 2 == 0:
+            return self.a
+        else:
+            return self.b
 
 def ring_pattern(color1, color2):
     return Ring(color1, color2)
@@ -28,13 +31,13 @@ class RingTestCase(unittest.TestCase):
         self.assertEqual(pattern.a, WHITE)
         self.assertEqual(pattern.b, BLACK)
 
-    #def test_a_gradient_linearly_interpolates_between_colors(self):
-        #pattern = gradient_pattern(WHITE, BLACK)
-#
-        #self.assertEqual(pattern.pattern_at(point(0, 0, 0)), WHITE)
-        #self.assertEqual(pattern.pattern_at(point(0.25, 0, 0)), color(0.75, 0.75, 0.75))
-        #self.assertEqual(pattern.pattern_at(point(0.5, 0, 0)), color(0.5, 0.5, 0.5))
-        #self.assertEqual(pattern.pattern_at(point(0.75, 0, 0)), color(0.25, 0.25, 0.25))
+    def test_a_ring_should_extend_in_both_x_and_z(self):
+        pattern = ring_pattern(WHITE, BLACK)
+
+        self.assertEqual(pattern.pattern_at(point(0, 0, 0)), WHITE)
+        self.assertEqual(pattern.pattern_at(point(1, 0, 0)), BLACK)
+        self.assertEqual(pattern.pattern_at(point(0, 0, 1)), BLACK)
+        self.assertEqual(pattern.pattern_at(point(0.708, 0, 0.708)), BLACK)
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
