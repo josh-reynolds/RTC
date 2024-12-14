5e885b001e088d046b0a4f22e8f26739580a6931
josh-reynolds <josh.reynolds@frontier.com>
2024-12-14 13:08:28 -0800

    Test render of nested patterns

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ from spheres import sphere
 from worlds import world
 from lights import point_light
 from cameras import camera
-from transformations import view_transform, translation, rotation_x, rotation_z, scaling
+from transformations import view_transform, translation, rotation_x, rotation_y, rotation_z, scaling
 from planes import plane
 from stripes import stripe_pattern
 from gradients import gradient_pattern
@@ -17,45 +17,49 @@ from radial_gradients import radial_gradient_pattern
 start_time = datetime.now()
 
 floor = plane()
-floor.material.pattern = radial_gradient_pattern(RED, BLUE)
-floor.material.pattern.set_transform(translation(0, 0.1, 0) * scaling(3, 3, 3))
+p1 = stripe_pattern(BLACK, WHITE)
+p1.set_transform(rotation_y(math.pi/4))
+p2 = stripe_pattern(YELLOW, BLUE)
+p2.set_transform(rotation_y(-math.pi/4))
+floor.material.pattern = checker_pattern(p1, p2)
+floor.material.pattern.set_transform(translation(0, 0.1, 0))
 
-middle = sphere()
-middle.transform = translation(-0.5, 1, 0.5) * rotation_z(math.pi/4)
-middle.material.diffuse = 0.7
-middle.material.specular = 0.3
-middle.material.pattern = stripe_pattern(WHITE, BLACK)
-
-right = sphere()
-right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5)
-right.material.diffuse = 0.7
-right.material.specular = 0.3
-right.material.pattern = gradient_pattern(MAGENTA, GREEN)
-right.material.pattern.set_transform(translation(1, 0, 0) * scaling(2, 1, 1))
-
-left = sphere()
-left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
-left.material.diffuse = 0.7
-left.material.specular = 0.3
-left.material.pattern = ring_pattern(checker_pattern(WHITE, BLACK), 
-                                     YELLOW)
-left.material.pattern.set_transform(scaling(0.1, 0.1, 0.1) * rotation_x(math.pi/2))
+#middle = sphere()
+#middle.transform = translation(-0.5, 1, 0.5) * rotation_z(math.pi/4)
+#middle.material.diffuse = 0.7
+#middle.material.specular = 0.3
+#middle.material.pattern = stripe_pattern(WHITE, BLACK)
+#
+#right = sphere()
+#right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5)
+#right.material.diffuse = 0.7
+#right.material.specular = 0.3
+#right.material.pattern = gradient_pattern(MAGENTA, GREEN)
+#right.material.pattern.set_transform(translation(1, 0, 0) * scaling(2, 1, 1))
+#
+#left = sphere()
+#left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
+#left.material.diffuse = 0.7
+#left.material.specular = 0.3
+#left.material.pattern = ring_pattern(checker_pattern(WHITE, BLACK), 
+                                     #YELLOW)
+#left.material.pattern.set_transform(scaling(0.1, 0.1, 0.1) * rotation_x(math.pi/2))
 
 w = world()
 w.objects.append(floor)
-w.objects.append(middle)
-w.objects.append(right)
-w.objects.append(left)
+#w.objects.append(middle)
+#w.objects.append(right)
+#w.objects.append(left)
 w.light = point_light(point(-10, 10, -10), WHITE)
 
-cam = camera(600, 300, math.pi/3)
+cam = camera(200, 100, math.pi/3)
 cam.transform = view_transform(point(0, 1.5, -5),
                                point(0, 1, 0),
                                vector(0, 1, 0))
 
 image = cam.render(w)
 
-f = open("./output/pattern_render_8.ppm", "w")
+f = open("./output/pattern_render_9.ppm", "w")
 lines = image.to_ppm()
 for line in lines:
     f.write(line + "\n")
