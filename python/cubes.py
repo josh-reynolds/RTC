1858193a6edb98e7af5b5b100002a45ef6125f54
josh-reynolds <josh.reynolds@frontier.com>
2024-12-19 11:48:55 -0800

    Basic version of Cube.local_intersect()

--- python/cubes.py
+++ python/cubes.py
@@ -1,15 +1,28 @@
 # to run tests: python -m unittest -v cubes
 
+import math
 import unittest
 import materials
 import shapes
+from rays import ray
+from tuples import point, vector
+from intersections import intersections, intersection
+from utils import EPSILON
 
 class Cube(shapes.Shape):
     def __init__(self):
         shapes.Shape.__init__(self)
 
     def local_intersect(self, r):
-        pass
+        xtmin, xtmax = check_axis(r.origin.x, r.direction.x)
+        ytmin, ytmax = check_axis(r.origin.y, r.direction.y)
+        ztmin, ztmax = check_axis(r.origin.z, r.direction.z)
+
+        tmin = max(xtmin, ytmin, ztmin)
+        tmax = min(xtmax, ytmax, ztmax)
+
+        return intersections(intersection(tmin, self),
+                             intersection(tmax, self))
 
     def local_normal_at(self, pt):
         pass
@@ -17,11 +30,72 @@ class Cube(shapes.Shape):
 def cube():
     return Cube()
 
+def check_axis(origin, direction):
+    tmin_numerator = (-1 - origin)
+    tmax_numerator = (1 - origin)
+
+    if abs(direction) >= EPSILON:
+        tmin = tmin_numerator / direction
+        tmax = tmax_numerator / direction
+    else:
+        tmin = tmin_numerator * math.inf
+        tmax = tmax_numerator * math.inf
+
+    if tmin > tmax:
+        tmin, tmax = tmax, tmin
+
+    return (tmin, tmax)
+
 class CubeTestCase(unittest.TestCase):
     def test_a_cube_is_a_shape(self):
         c = cube()
 
         self.assertTrue(isinstance(c, shapes.Shape))
+
+    def test_a_ray_intersects_a_cube(self):
+        c = cube()
+
+        r = ray(point(5, 0.5, 0), vector(-1, 0, 0))
+        xs = c.local_intersect(r)
+        self.assertEqual(len(xs), 2)
+        self.assertEqual(xs[0].t, 4)
+        self.assertEqual(xs[1].t, 6)
+
+        r = ray(point(-5, 0.5, 0), vector(1, 0, 0))
+        xs = c.local_intersect(r)
+        self.assertEqual(len(xs), 2)
+        self.assertEqual(xs[0].t, 4)
+        self.assertEqual(xs[1].t, 6)
+
+        r = ray(point(0.5, 5, 0), vector(0, -1, 0))
+        xs = c.local_intersect(r)
+        self.assertEqual(len(xs), 2)
+        self.assertEqual(xs[0].t, 4)
+        self.assertEqual(xs[1].t, 6)
+
+        r = ray(point(0.5, -5, 0), vector(0, 1, 0))
+        xs = c.local_intersect(r)
+        self.assertEqual(len(xs), 2)
+        self.assertEqual(xs[0].t, 4)
+        self.assertEqual(xs[1].t, 6)
+
+        r = ray(point(0.5, 0, 5), vector(0, 0, -1))
+        xs = c.local_intersect(r)
+        self.assertEqual(len(xs), 2)
+        self.assertEqual(xs[0].t, 4)
+        self.assertEqual(xs[1].t, 6)
+
+        r = ray(point(0.5, 0, -5), vector(0, 0, 1))
+        xs = c.local_intersect(r)
+        self.assertEqual(len(xs), 2)
+        self.assertEqual(xs[0].t, 4)
+        self.assertEqual(xs[1].t, 6)
+
+        r = ray(point(0, 0.5, 0), vector(0, 0, 1))
+        xs = c.local_intersect(r)
+        self.assertEqual(len(xs), 2)
+        self.assertEqual(xs[0].t, -1)
+        self.assertEqual(xs[1].t, 1)
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
