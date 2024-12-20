d526628097783a441fcfe156b85eb9b4f76770c6
josh-reynolds <josh.reynolds@frontier.com>
2024-12-20 11:56:03 -0800

    Test render of infinite/uncapped cylinders

--- python/main.py
+++ python/main.py
@@ -16,6 +16,7 @@ from radial_gradients import radial_gradient_pattern
 from blended_patterns import blend_pattern
 from utils import image_to_file
 from cubes import cube
+from cylinders import cylinder
 
 start_time = datetime.now()
 
@@ -27,7 +28,7 @@ floor.material.pattern = blend_pattern(p1, p2)
 floor.material.pattern.set_transform(rotation_y(math.pi/4) * translation(0, 0.1, 0))
 floor.material.reflective = 0.9
 
-middle = sphere()
+middle = cylinder()
 middle.transform = translation(-0.5, 1, 0.5) * rotation_z(math.pi/4)
 middle.material.diffuse = 0.2
 middle.material.ambient = 0.2
@@ -38,14 +39,14 @@ middle.material.transparency = 0.9
 middle.material.refractive_index = 1.5
 middle.material.color = color(0, 0, 0.1)
 
-right = cube()
+right = cylinder()
 right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5)
 right.material.diffuse = 0.7
 right.material.specular = 0.3
 right.material.pattern = gradient_pattern(MAGENTA, GREEN)
 right.material.pattern.set_transform(translation(1, 0, 0) * scaling(2, 1, 1))
 
-left = cube()
+left = cylinder()
 left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
 left.material.diffuse = 0.7
 left.material.specular = 0.3
@@ -67,7 +68,7 @@ cam.transform = view_transform(point(0, 1.5, -5),
                                vector(0, 1, 0))
 
 image = cam.render(w)
-image_to_file(image, "./output/cubes.ppm")
+image_to_file(image, "./output/cylinders.ppm")
 
 end_time = datetime.now()
 print("Image size: {} x {}".format(cam.hsize, cam.vsize))
