44a07b2b5d16637024770f05fe6e7867ca284a88
josh-reynolds <josh.reynolds@frontier.com>
2025-01-01 17:30:22 -0800

    Implementing fan_triangulation() and handling of polygonal face data from OBJ files

--- python/obj_files.py
+++ python/obj_files.py
@@ -14,21 +14,36 @@ class ObjFileParser:
 def parse_obj_file(file):
     parser = ObjFileParser()
     for line in file:
-        if line[0] == 'v':
-            tokens = line.split()
+        tokens = line.split()
+        if tokens and tokens[0] == 'v':
             parser.vertices.append(tuples.point(float(tokens[1]), 
                                                 float(tokens[2]), 
                                                 float(tokens[3])))
-        elif line[0] == 'f':
-            tokens = line.split()
-            tri = triangles.triangle(parser.vertices[int(tokens[1])],
-                                     parser.vertices[int(tokens[2])],
-                                     parser.vertices[int(tokens[3])])
-            parser.default_group.add_child(tri)
+        elif tokens and tokens[0] == 'f' and len(tokens) >= 4:
+            vertices = ['']    # need dummy entry here too
+            for token in tokens[1:]:
+                vertices.append(parser.vertices[int(token)])
+
+            if len(tokens) == 4:
+                tri = triangles.triangle(vertices[1], vertices[2], vertices[3])
+                parser.default_group.add_child(tri)
+            else:
+                tris = fan_triangulate(vertices)
+                for tri in tris:
+                    parser.default_group.add_child(tri)
         else:
             parser.ignored += 1
 
     return parser
+
+def fan_triangulate(vertices):
+    tris = []
+
+    for i in range(2, len(vertices)-1):
+        tri = triangles.triangle(vertices[1], vertices[i], vertices[i+1])
+        tris.append(tri)
+
+    return tris
 
 class ObjFileTestCase(unittest.TestCase):
     def test_ignoring_unrecognized_lines(self):
@@ -75,7 +90,31 @@ class ObjFileTestCase(unittest.TestCase):
         self.assertEqual(t2.p1, parser.vertices[1])
         self.assertEqual(t2.p2, parser.vertices[3])
         self.assertEqual(t2.p3, parser.vertices[4])
-        
+
+    def test_triangulating_polygons(self):
+        file = ["v -1 1 0\n",
+                "v -1 0 0\n",
+                "v 1 0 0\n",
+                "v 1 1 0\n",
+                "v 0 2 0\n",
+                "\n",
+                "f 1 2 3 4 5\n"]
+
+        parser = parse_obj_file(file)
+        g = parser.default_group
+        t1 = g.contents[0]
+        t2 = g.contents[1]
+        t3 = g.contents[2]
+
+        self.assertEqual(t1.p1, parser.vertices[1])
+        self.assertEqual(t1.p2, parser.vertices[2])
+        self.assertEqual(t1.p3, parser.vertices[3])
+        self.assertEqual(t2.p1, parser.vertices[1])
+        self.assertEqual(t2.p2, parser.vertices[3])
+        self.assertEqual(t2.p3, parser.vertices[4])
+        self.assertEqual(t3.p1, parser.vertices[1])
+        self.assertEqual(t3.p2, parser.vertices[4])
+        self.assertEqual(t3.p3, parser.vertices[5])
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
