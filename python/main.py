3e115735ed99540ae7ad54bb0932c9326bd291a2
josh-reynolds <josh.reynolds@frontier.com>
2025-01-05 14:08:03 -0800

    Another render of a CSG object

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 import math
 from datetime import datetime
 from tuples import point, vector
-from colors import WHITE, BLACK, RED, GREEN, BLUE, MAGENTA, YELLOW, color
+from colors import WHITE, BLACK, RED, GREEN, BLUE, MAGENTA, YELLOW, DARK_GREY, LIGHT_GREY, color
 from spheres import sphere, glass_sphere
 from worlds import world
 from lights import point_light
@@ -26,41 +26,56 @@ from csgs import csg
 start_time = datetime.now()
 
 floor = plane()
-floor.material.color = BLUE
+floor.material.color = DARK_GREY
 
 wall = plane()
 wall.set_transform(translation(0, 0, 15) * rotation_x(math.pi/4))
-wall.material.pattern = gradient_pattern(RED, YELLOW)
+wall.material.color = LIGHT_GREY
 
-s1 = glass_sphere()
-s1.set_transform(translation(0.7, 1, 0))
+c = cube()
+c.material.color = MAGENTA
+c.set_transform(translation(0, 1, 0) * 
+                rotation_z(math.pi/3) * 
+                rotation_x(math.pi/3) *
+                scaling(0.5, 0.5, 0.5))
 
-s2 = glass_sphere()
-s2.set_transform(translation(-0.7, 1, 0))
+cy1 = cylinder()
+cy1.minimum = -2
+cy1.maximum = 2
+cy1.closed = True
+cy1.material.color= GREEN
+cy1.set_transform(translation(0, 1, 0) * 
+                 rotation_z(math.pi/3) * 
+                 rotation_x(math.pi/3) *
+                 scaling(0.3, 0.3, 0.3))
 
-s3 = glass_sphere()
-s3.set_transform(translation(0, 1, 0))
+cy2 = cylinder()
+cy2.minimum = -2
+cy2.maximum = 2
+cy2.closed = True
+cy2.material.color= GREEN
+cy2.set_transform(translation(0, 1, 0) * 
+                 rotation_z(math.pi/3) * 
+                 rotation_x(math.pi/3 + math.pi/2) *
+                 scaling(0.3, 0.3, 0.3))
 
-c1 = csg("union", s1, s2)
-c2 = csg("union", c1, s3)
+c1 = csg("difference", c, cy1)
+c2 = csg("difference", c1, cy2)
 
 w = world()
 w.objects.append(floor)
 w.objects.append(wall)
-#w.objects.append(s1)
-#w.objects.append(s2)
-#w.objects.append(s3)
 w.objects.append(c2)
 
 w.light = point_light(point(-10, 10, -10), WHITE)
 
-cam = camera(300, 150, math.pi/3)
+cam = camera(400, 200, math.pi/3)
 cam.transform = view_transform(point(0, 1.5, -5),
                                point(0, 1, 0),
                                vector(0, 1, 0))
 
 image = cam.render(w)
-image_to_file(image, "./output/csg.ppm")
+image_to_file(image, "./output/csg_2.ppm")
 
 end_time = datetime.now()
 print("Image size: {} x {}".format(cam.hsize, cam.vsize))
