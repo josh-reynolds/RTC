c80bd8b4dc26a51ef3550d5122f0f5d86fa3f840
josh-reynolds <josh.reynolds@frontier.com>
2025-01-04 19:04:53 -0800

    Running test renders with and without CSG grouping for comparison

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ import math
 from datetime import datetime
 from tuples import point, vector
 from colors import WHITE, BLACK, RED, GREEN, BLUE, MAGENTA, YELLOW, color
-from spheres import sphere
+from spheres import sphere, glass_sphere
 from worlds import world
 from lights import point_light
 from cameras import camera
@@ -21,30 +21,46 @@ from cones import cone
 from groups import group
 from triangles import triangle
 from obj_files import parse_obj_file
+from csgs import csg
 
 start_time = datetime.now()
 
-with open("./obj/gourd.obj", "r") as file:
-    lines = file.readlines()
+floor = plane()
+floor.material.color = BLUE
 
-parser = parse_obj_file(lines)
-g = parser.obj_to_group()
-g.set_transform(rotation_y(math.pi/3) * 
-                rotation_z(-math.pi/3) *
-                translation(-0.3,0,0))
+wall = plane()
+wall.set_transform(translation(0, 0, 15) * rotation_x(math.pi/4))
+wall.material.pattern = gradient_pattern(RED, YELLOW)
+
+s1 = glass_sphere()
+s1.set_transform(translation(0.7, 1, 0))
+
+s2 = glass_sphere()
+s2.set_transform(translation(-0.7, 1, 0))
+
+s3 = glass_sphere()
+s3.set_transform(translation(0, 1, 0))
+
+c1 = csg("union", s1, s2)
+c2 = csg("union", c1, s3)
 
 w = world()
-w.objects.append(g)
+w.objects.append(floor)
+w.objects.append(wall)
+#w.objects.append(s1)
+#w.objects.append(s2)
+#w.objects.append(s3)
+w.objects.append(c2)
 
 w.light = point_light(point(-10, 10, -10), WHITE)
 
-cam = camera(100, 50, math.pi/3)
+cam = camera(300, 150, math.pi/3)
 cam.transform = view_transform(point(0, 1.5, -5),
                                point(0, 1, 0),
                                vector(0, 1, 0))
 
 image = cam.render(w)
-image_to_file(image, "./output/obj_file_sample_5.ppm")
+image_to_file(image, "./output/csg.ppm")
 
 end_time = datetime.now()
 print("Image size: {} x {}".format(cam.hsize, cam.vsize))
